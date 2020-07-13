c2ad494c0fd26432e639fe28f7f01b7ef7d4aed5
Bence <m.bence05@gmail.com>
2020-07-13 16:33:43 +0200

    zmena config servera na ENV premenne a zaroven premenovanie adries v a href

--- resources/bootstrap.js
+++ resources/bootstrap.js
@@ -12,6 +12,11 @@ module.exports = {
     profiles: {
         active: "dev"
     },
+    servers: {
+        mobile: process.env.MOBILE_SERVER,
+        desktop: process.env.DESKTOP_SERVER,
+        management: process.env.MANAGEMENT_SERVER
+    },
     server: {
         port: process.env.PORT
     }
